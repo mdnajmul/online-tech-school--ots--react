4f3fd9697e22186b52d7777af04d67f801e351c1
mdnajmul <mdnajmulovi@gmail.com>
2021-10-13 12:50:24 +0600

    set dynamic star ratings

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Rating from "react-rating";
 import "./Course.css";
 
 const Course = (props) => {
@@ -6,8 +7,19 @@ const Course = (props) => {
   return (
     <div className="course">
       <img src={img} className="img-fluid" alt="" />
-      <h3>{name}</h3>
-      <p>{description}</p>
+      <h3 className="text-center fs-5 fw-bold ps-3 pe-3">{name}</h3>
+      <p className="text-center fs-6 ps-3 pe-3">{description}</p>
+      <p className="ps-3 pe-3">
+        <hr />
+      </p>
+      <p className="text-center">
+        <Rating
+          initialRating={rating}
+          emptySymbol="far fa-star icon-color"
+          fullSymbol="fas fa-star icon-color"
+          readonly
+        ></Rating>
+      </p>
       <small>{price}</small>
     </div>
   );
